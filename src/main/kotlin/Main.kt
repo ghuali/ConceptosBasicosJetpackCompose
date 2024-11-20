5f23f7cbc089e2adb8f4971b0aeadcab2c83fed9
ghuali <guaghuali@gmail.com>
2024-11-20 13:08:49 +0000

    Completado primer codelab hasta el 8

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,6 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -10,63 +8,101 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 @Composable
-fun aplicacion(listagayming : List<String> = listOf(
-    "Soy una lista gamer",
-    "Soy una lista muy gaymer",
-    "Soy una lista poco gaymer",
-    "Soy una lista que se llama Ang"
-)) {
-    var enseñar by remember { mutableStateOf("Show more") }
+@Preview
+fun App() {
+    MaterialTheme {
+        var showInicio by remember { mutableStateOf(true) }
+        if (showInicio) {
+            Inicio{showInicio = false}
+        } else {
+            SegundaPantalla()
+        }
+    }
+}
 
+@Composable
+fun Inicio(changeShowInicio: () -> Unit){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text("gaypantalla de inicio")
+        Button(
+            onClick = changeShowInicio,
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xff9b88f6))
+        ){
+            Text(
+                text = "sigue",
+                color = Color.White
+            )
+        }
+    }
 
+}
 
-    Column (modifier = Modifier.padding(5.dp)
-                                .fillMaxWidth()){
-        Row {
-            for (I in listagayming) {
-                Text(I)
-                Button(
-                    onClick = {
-                    Text("show less")}
-                ) {
-                    Text(enseñar)
+@Composable
+fun SegundaPantalla(){
+    val textos: MutableList<String> = mutableListOf(
+        "te odio Andrei",
+        "te amo Samu",
+        "te quiero Alvaro",
+        "AMO A ADAY(no soy gay)"
+    )
+    Column {
+        textos.forEach{ texto ->
+            Tarjeta(texto)
+        }
+    }
+}
 
-                }
+@Composable
+fun Tarjeta(texto: String = ""){
+    var expanded by remember { mutableStateOf(0.dp) }
+    var isExpanded by remember { mutableStateOf(false) }
+    Card(
+        modifier = Modifier.padding(16.dp).fillMaxWidth(),
+        backgroundColor = Color(0xfff5e6cc),
+        elevation = 2.dp,
+        shape = MaterialTheme.shapes.small,
+    ){
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(start = 8.dp, end = 8.dp, bottom = expanded)
+        ) {
+            Text(
+                modifier = Modifier.weight(1f),
+                text = texto
+            )
+            Button(
+                onClick = {
+                    if (isExpanded) {
+                        expanded = 0.dp
+                        isExpanded = !isExpanded
+                    } else{
+                        expanded = 120.dp
+                        isExpanded = !isExpanded
+                    }
+                },
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xffc8f4d6))
+            ){
+                Text(
+                    text = (if (isExpanded) "Show less" else "Show more"),
+                    color = Color.White
+                )
             }
         }
     }
 }
 
-
-//@Composable
-//fun App() {
-//    var estado by remember { mutableStateOf(true) }
-//    Column(
-//        modifier = Modifier.fillMaxSize(),
-//        verticalArrangement = Arrangement.Center,
-//        horizontalAlignment = Alignment.CenterHorizontally
-//    ) {
-//        Text("wassup bb girl")
-//        Button(onClick = {
-//            if (estado) {
-
-//            }
-//        }) {
-//            Text("Comenzamos")
-//        }
-//    }
-
-
-
-
-
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
-        aplicacion()
+        App()
     }
 }
