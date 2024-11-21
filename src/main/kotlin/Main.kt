8ac6ae0816608dd4de29c78d666bc1a6100d43a6
ghuali <guaghuali@gmail.com>
2024-11-21 08:30:44 +0000

    comenzado animaciones

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,6 @@
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
@@ -5,10 +8,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
@@ -55,6 +60,7 @@ fun SegundaPantalla(){
         "te quiero Alvaro",
         "AMO A ADAY(no soy gay)"
     )
+
     Column {
         textos.forEach{ texto ->
             Tarjeta(texto)
@@ -64,8 +70,19 @@ fun SegundaPantalla(){
 
 @Composable
 fun Tarjeta(texto: String = ""){
-    var expanded by remember { mutableStateOf(0.dp) }
-    var isExpanded by remember { mutableStateOf(false) }
+    var expanded by rememberSaveable { mutableStateOf(0.dp) }
+    var isExpanded by rememberSaveable { mutableStateOf(false) }
+    val animationPadding by
+        animateDpAsState(
+            if (isExpanded) 48.dp else 0.dp,
+            animationSpec = spring(
+                dampingRatio = Spring.DampingRatioMediumBouncy,
+                stiffness = Spring.StiffnessLow
+            )
+        )
+
+    val currentPadding = animationPadding.coerceAtLeast(0.dp)
+
     Card(
         modifier = Modifier.padding(16.dp).fillMaxWidth(),
         backgroundColor = Color(0xfff5e6cc),
